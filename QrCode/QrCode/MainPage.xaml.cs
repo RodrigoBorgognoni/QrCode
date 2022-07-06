39e331013ab3296ecfadc95d77679b30f7a060ec
RodrigoBorgognoni <80656730+RodrigoBorgognoni@users.noreply.github.com>
2022-07-06 16:49:31 -0300

    Adicionar arquivos de projeto.

---
+++ QrCode/QrCode/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+
+namespace QrCode
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnScan_Clicked(object sender, EventArgs e)
+        {
+
+            var ScannerPage = new ZXingScannerPage();
+
+            ScannerPage.OnScanResult += (result) => {
+                // Parar de escanear
+                ScannerPage.IsScanning = false;
+                
+                // Puxa o valor do código escaneado
+                Device.BeginInvokeOnMainThread(() => {
+                    Navigation.PopAsync();
+                    txtBarcode.Text = result.Text;
+                });
+            };
+
+
+            await Navigation.PushAsync(ScannerPage);
+        }
+    }
+}
